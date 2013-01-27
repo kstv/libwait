e70d16cb7fec6bbe7b7142d19577bed9790850e4
pagxir <pagxir@gmail.com>
2013-01-27 18:38:36 +0800

    fix Android.mk for compile in android source project

--- Android.mk
+++ Android.mk
@@ -4,8 +4,8 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := libwait
 LOCAL_SRC_FILES := src/callout.cpp src/module.cpp \
 				   src/utils.cpp src/slotwait.cpp \
-				   src/platform-linux.cpp src/slotsock-linux.cpp \
-LOCAL_CFLAGS  := -Iinclude
-LOCAL_CXXFLAGS  := -Iinclude
+				   src/platform-linux.cpp src/slotsock-linux.cpp
+LOCAL_C_INCLUDES += $(JNI_H_INCLUDE) $(LOCAL_PATH)/include
+LOCAL_CXX_INCLUDES += $(JNI_H_INCLUDE) $(LOCAL_PATH)/include
 include $(BUILD_STATIC_LIBRARY)
 
